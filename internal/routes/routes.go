29f954d5dc5a63f40e3a045fd4af1d3c4a282c4e
komogortzef <komogortzef@gmail.com>
2024-05-04 23:12:50 +0700

    отпр

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -15,18 +15,15 @@ import (
 // 	Mux.HandleFunc("/update/", handler.SaveToMem)
 // }
 
-func SetRouter(storeType string) chi.Router {
+func SetRouter() chi.Router {
 	r := chi.NewRouter()
 
-	switch {
-	default:
-		h := handlers.NewHandler(storage.MemStorage{})
-		r.Get("/", h.ShowAll)
-		r.Route("/", func(r chi.Router) {
-			r.Get("/value/{tp}/{name}", h.GetMetric)
-			r.Post("/update/{tp}/{name}/{val}", h.SaveToMem)
-		})
-	}
+	h := handlers.NewHandler(storage.MemStorage{})
+	r.Get("/", h.ShowAll)
+	r.Route("/", func(r chi.Router) {
+		r.Get("/value/{tp}/{name}", h.GetMetric)
+		r.Post("/update/{tp}/{name}/{val}", h.SaveToMem)
+	})
 
 	return r
 }
