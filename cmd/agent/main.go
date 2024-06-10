5c5e52cf7ede645ed218dce70c3ec14f82f33231
komogortzef <165701323+komogortzef@users.noreply.github.com>
2024-06-10 20:08:30 +0700

    refactor: config

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"metrics/internal/agent"
 	c "metrics/internal/config"
-	l "metrics/internal/logger"
+	log "metrics/internal/logger"
 
 	"go.uber.org/zap"
 )
@@ -11,10 +11,10 @@ import (
 func main() {
 	agent, err := c.Configure(&agent.SelfMonitor{}, c.WithEnvCmd)
 	if err != nil {
-		l.Fatal("agent config error", zap.Error(err))
+		log.Fatal("agent config error", zap.Error(err))
 	}
 
 	if err = agent.Run(); err != nil {
-		l.Fatal("agent run error", zap.Error(err))
+		log.Fatal("agent run error", zap.Error(err))
 	}
 }
